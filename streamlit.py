bbcbd7f68e00ddcac4efcf83e07355ace643146b
Shubham Chambhare <Shubhamchambhare654@gmail.com>
2022-01-17 23:23:34 +0530

    init

---
+++ streamlit.py
@@ -0,0 +1,54 @@
+from turtle import width
+import streamlit as st
+import tweepy as tw
+import pandas as pd
+from transformers import pipeline
+from PIL import Image
+import plotly.graph_objects as go
+
+
+consumer_key = 'OCgWzDW6PaBvBeVimmGBqdAg1'
+consumer_secret = 'tBKnmyg5Jfsewkpmw74gxHZbbZkGIH6Ee4rsM0lD1vFL7SrEIM'
+access_token = '1449663645412065281-LNjZoEO9lxdtxPcmLtM35BRdIKYHpk'
+access_token_secret = 'FL3SGsUWSzPVFnG7bNMnyh4vYK8W1SlABBNtdF7Xcbh7a'
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+classifier = pipeline('sentiment-analysis')
+image = Image.open('twitter_image.jpeg')
+
+
+st.title('Live Twitter Sentiment Analysis')
+st.markdown('This app uses tweepy to get tweets from twitter based on the input name/phrase. It then processes the tweets through HuggingFace transformers pipeline function for sentiment analysis. The resulting sentiments and corresponding tweets are then put in a dataframe for display which is what you see as result.')
+st.image(image)
+
+def run():
+
+    with st.form(key='Enter name'):
+        search_words = st.text_input('Enter the name for which you want to know the sentiment')
+        number_of_tweets = st.number_input('Enter the number of latest tweets for which you want to know the sentiment(Maximum 50 tweets)', 0,50,10)
+        submit_button = st.form_submit_button(label='Submit')
+    if submit_button:
+        tweets =tw.Cursor(api.search_tweets,q=search_words,lang="en").items(number_of_tweets)
+        tweet_list = [i.text for i in tweets]
+        p = [i for i in classifier(tweet_list)]
+        q=[p[i]['label'] for i in range(len(p))]
+
+        num_negative = q.count('NEGATIVE')
+        print(num_negative)
+        labels = ['NEGATIVE', 'POSITIVE']
+        val = [num_negative, len(q)-num_negative]
+        fig = go.Figure(
+            go.Pie(
+            labels = labels,
+            values = val,
+            hoverinfo = "label+percent",
+            textinfo = "value"
+        ))
+        
+        df = pd.DataFrame(list(zip(tweet_list, q)),columns =['Latest '+str(number_of_tweets)+' Tweets'+' on '+search_words, 'sentiment'])
+        st.write(df)
+        st.plotly_chart(fig)
+
+if __name__=='__main__':
+    run()
